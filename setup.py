5fee42a913c62ac6813c2727571b9f0f69de95fd
pierre paci <pierre.paci@gmail.com>
2018-07-01 11:18:17 +0200

    updates setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup
 
+with open("Readme.md") as f:
+    long_desc = f.read()
+
 setup(
         name='mojito',
         version='0.3.0',
         packages=['mojito'],
-        url='smart-metric.com',
         license='',
         author='Pierre Paci',
         author_email='',
         description='Mocking temporal data made easy !',
+        long_description=long_desc,
+        url="https://github.com/PPACI/mojito",
+        long_description_content_type="text/markdown",
+        classifiers=(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Topic :: Scientific/Engineering :: Mathematics",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+            "Topic :: Scientific/Engineering"
+        ),
         install_requires=[
             "numpy==1.13.3",
             "scipy==1.1.0"
